e648a218b664e8c23c378c8af602a7ac89a1cf74
mrdevil786 <suhailsaeedme@gmail.com>
2024-10-07 00:31:29 +0530

    Added Invoice

--- resources/views/admin/invoices/create-edit-view.blade.php
+++ resources/views/admin/invoices/create-edit-view.blade.php
@@ -59,7 +59,7 @@
                             </div>
                         </div>
 
-                        <div class="form-row">
+                        <div class="form-row mt-5">
                             <div class="col-lg-12 mb-3">
                                 <h4>Items</h4>
                                 <div id="items">
@@ -91,7 +91,9 @@
                                         </div>
                                     </div>
                                 </div>
-                                <button type="button" id="add-item" class="btn btn-primary">Add Item</button>
+                                <div class="text-end">
+                                    <button type="button" id="add-item" class="btn btn-primary"><i class="fe fe-plus-square"></i> Add Item</button>
+                                </div>
                             </div>
                         </div>
 
@@ -116,10 +118,25 @@
 @section('custom-script')
     <script>
         $(document).ready(function() {
+            function updatePrice(itemDiv) {
+                const quantityInput = itemDiv.find('input[name$="[quantity]"]');
+                const priceInput = itemDiv.find('input[name$="[price]"]');
+                const selectedProduct = itemDiv.find('select[name$="[product_id]"] option:selected');
+                const productPrice = parseFloat(selectedProduct.data('price')) || 0;
+                const quantity = parseFloat(quantityInput.val()) || 1;
+
+                priceInput.val((productPrice * quantity).toFixed(2));
+            }
+
             $('select[name="items[0][product_id]"]').change(function() {
                 const selectedOption = $(this).find('option:selected');
                 const priceInput = $(this).closest('.item').find('input[name="items[0][price]"]');
                 priceInput.val(selectedOption.data('price'));
+            });
+
+            $(document).on('input', 'input[name$="[quantity]"]', function() {
+                const itemDiv = $(this).closest('.item');
+                updatePrice(itemDiv);
             });
 
             $('#type').change(function() {
@@ -134,27 +151,27 @@
             $('#add-item').click(function() {
                 const itemCount = $('.item').length;
                 const itemDiv = $(`<div class="item mb-3">
-                    <div class="row">
-                        <div class="col-md-4 mb-3">
-                            <label class="form-label" for="product_id">Product</label>
-                            <select class="form-select form-control" name="items[${itemCount}][product_id]" required>
-                                <option value="">Select Product</option>
-                                @foreach ($products as $product)
-                                    <option value="{{ $product->id }}" data-price="{{ $product->price }}">{{ $product->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label class="form-label" for="quantity">Quantity</label>
-                            <input type="number" class="form-control" name="items[${itemCount}][quantity]" value="1" required>
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label class="form-label" for="price">Price</label>
-                            <input type="number" class="form-control" name="items[${itemCount}][price]" readonly required>
-                        </div>
+                <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <label class="form-label" for="product_id">Product</label>
+                        <select class="form-select form-control" name="items[${itemCount}][product_id]" required>
+                            <option value="">Select Product</option>
+                            @foreach ($products as $product)
+                                <option value="{{ $product->id }}" data-price="{{ $product->price }}">{{ $product->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
-                    <button type="button" class="btn btn-danger remove-item">Remove</button>
-                </div>`);
+                    <div class="col-md-4 mb-3">
+                        <label class="form-label" for="quantity">Quantity</label>
+                        <input type="number" class="form-control" name="items[${itemCount}][quantity]" value="1" required>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label class="form-label" for="price">Price</label>
+                        <input type="number" class="form-control" name="items[${itemCount}][price]" readonly required>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-danger remove-item"><i class="fe fe-trash"></i></button>
+            </div>`);
 
                 $('#items').append(itemDiv);
 
@@ -162,6 +179,11 @@
                     const selectedOption = $(this).find('option:selected');
                     const priceInput = $(this).closest('.item').find('input[name$="[price]"]');
                     priceInput.val(selectedOption.data('price'));
+                    updatePrice(itemDiv);
+                });
+
+                itemDiv.find('input[name$="[quantity]"]').on('input', function() {
+                    updatePrice(itemDiv);
                 });
 
                 itemDiv.find('.remove-item').click(function() {
