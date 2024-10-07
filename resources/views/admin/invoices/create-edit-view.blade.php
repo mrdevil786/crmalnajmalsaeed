8d8cf2658de56389a5c4417ae590d720af69453b
Suhail Saeed Usmani <suhailsaeedme@gmail.com>
2024-10-07 13:10:41 +0530

    Fixed The Add Edit View Page

--- resources/views/admin/invoices/create-edit-view.blade.php
+++ resources/views/admin/invoices/create-edit-view.blade.php
@@ -83,16 +83,18 @@
                                                     value="1" required>
                                             </div>
 
+                                            <input type="hidden" class="form-control" name="items[0][price]" required>
+
                                             <div class="col-md-4 mb-3">
-                                                <label class="form-label" for="price">Price</label>
-                                                <input type="number" class="form-control" name="items[0][price]" readonly
-                                                    required>
+                                                <label class="form-label" for="total_price">Total Price</label>
+                                                <input type="text" class="form-control" id="total_price" readonly>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="text-end">
-                                    <button type="button" id="add-item" class="btn btn-primary"><i class="fe fe-plus-square"></i> Add Item</button>
+                                    <button type="button" id="add-item" class="btn btn-primary"><i
+                                            class="fe fe-plus-square"></i> Add Item</button>
                                 </div>
                             </div>
                         </div>
@@ -121,17 +123,18 @@
             function updatePrice(itemDiv) {
                 const quantityInput = itemDiv.find('input[name$="[quantity]"]');
                 const priceInput = itemDiv.find('input[name$="[price]"]');
+                const totalPriceInput = itemDiv.find('#total_price');
                 const selectedProduct = itemDiv.find('select[name$="[product_id]"] option:selected');
                 const productPrice = parseFloat(selectedProduct.data('price')) || 0;
                 const quantity = parseFloat(quantityInput.val()) || 1;
 
-                priceInput.val((productPrice * quantity).toFixed(2));
+                priceInput.val(productPrice.toFixed(2));
+                totalPriceInput.val((productPrice * quantity).toFixed(2));
             }
 
             $('select[name="items[0][product_id]"]').change(function() {
-                const selectedOption = $(this).find('option:selected');
-                const priceInput = $(this).closest('.item').find('input[name="items[0][price]"]');
-                priceInput.val(selectedOption.data('price'));
+                const itemDiv = $(this).closest('.item');
+                updatePrice(itemDiv);
             });
 
             $(document).on('input', 'input[name$="[quantity]"]', function() {
@@ -151,34 +154,32 @@
             $('#add-item').click(function() {
                 const itemCount = $('.item').length;
                 const itemDiv = $(`<div class="item mb-3">
-                <div class="row">
-                    <div class="col-md-4 mb-3">
-                        <label class="form-label" for="product_id">Product</label>
-                        <select class="form-select form-control" name="items[${itemCount}][product_id]" required>
-                            <option value="">Select Product</option>
-                            @foreach ($products as $product)
-                                <option value="{{ $product->id }}" data-price="{{ $product->price }}">{{ $product->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label class="form-label" for="quantity">Quantity</label>
-                        <input type="number" class="form-control" name="items[${itemCount}][quantity]" value="1" required>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label class="form-label" for="price">Price</label>
-                        <input type="number" class="form-control" name="items[${itemCount}][price]" readonly required>
-                    </div>
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <label class="form-label" for="product_id">Product</label>
+                    <select class="form-select form-control" name="items[${itemCount}][product_id]" required>
+                        <option value="">Select Product</option>
+                        @foreach ($products as $product)
+                            <option value="{{ $product->id }}" data-price="{{ $product->price }}">{{ $product->name }}</option>
+                        @endforeach
+                    </select>
                 </div>
-                <button type="button" class="btn btn-danger remove-item"><i class="fe fe-trash"></i></button>
-            </div>`);
+                <div class="col-md-4 mb-3">
+                    <label class="form-label" for="quantity">Quantity</label>
+                    <input type="number" class="form-control" name="items[${itemCount}][quantity]" value="1" required>
+                </div>
+                <input type="hidden" class="form-control" name="items[${itemCount}][price]" required>
+                <div class="col-md-4 mb-3">
+                    <label class="form-label" for="total_price">Total Price</label>
+                    <input type="text" class="form-control" id="total_price" readonly>
+                </div>
+            </div>
+            <button type="button" class="btn btn-danger remove-item"><i class="fe fe-trash"></i></button>
+        </div>`);
 
                 $('#items').append(itemDiv);
 
                 itemDiv.find('select[name^="items"][name$="[product_id]"]').change(function() {
-                    const selectedOption = $(this).find('option:selected');
-                    const priceInput = $(this).closest('.item').find('input[name$="[price]"]');
-                    priceInput.val(selectedOption.data('price'));
                     updatePrice(itemDiv);
                 });
 
