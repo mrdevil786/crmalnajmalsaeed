ea478774f86947f5213fde6e93682369938d135c
suhail-codenia <suhail.usmani@corewebconnections.com>
2025-02-21 11:36:07 +0530

    Remove Unused Delete Button Component

--- resources/views/Components/Buttons/delete-button.blade.php
+++
@@ -1,9 +0,0 @@
-<form action="{{ $route }}" method="POST" style="display: inline;">
-    @csrf
-    @method('DELETE')
-    <button type="submit"
-            class="btn btn-outline-danger btn-pill btn-sm"
-            onclick="return confirm('{{ $confirmMessage }}');">
-        <i class="fa fa-trash"></i>
-    </button>
-</form>
